a8e4388a813a48cd032e0e2b31361a25b099ce47
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-08-17 13:29:52 +0100

    commited

--- application/views/landing/home.php
+++ application/views/landing/home.php
@@ -115,7 +115,7 @@
                                 <h4 class="slider-item-caption-title">Save up to $150 on Your Next Laptop</h4>
                                 <p class="slider-item-caption-desc">I'm Not Gonna Pay A Lot For This Laptop.</p><a class="btn btn-lg btn-ghost btn-white" href="#">Shop Now</a>
                             </div>
-                            <img class="slider-item-img-right" src="img/test_slider/1.png" alt="Image Alternative text" title="Image Title" style="top: 60%; width: 56%;" />
+                            <img class="slider-item-img-right" src="<?= base_url('assets/landing/img/test_slider/1.png');?>" alt="Image Alternative text" title="Image Title" style="top: 60%; width: 56%;" />
                         </div>
                     </div>
                 </div>
@@ -128,7 +128,7 @@
                                 <h4 class="slider-item-caption-title">World Top Guitars from $350</h4>
                                 <p class="slider-item-caption-desc">Fill It To The Rim With Guitar.</p><a class="btn btn-lg btn-ghost btn-white" href="#">Shop Now</a>
                             </div>
-                            <img class="slider-item-img-left" src="img/test_slider/2.png" alt="Image Alternative text" title="Image Title" style="transform:translateY(-50%) rotate(14deg); width: 55%;" />
+                            <img class="slider-item-img-left" src="<?= base_url('assets/landing/img/test_slider/2.png');?>" alt="Image Alternative text" title="Image Title" style="transform:translateY(-50%) rotate(14deg); width: 55%;" />
                         </div>
                     </div>
                 </div>
@@ -141,7 +141,7 @@
                                 <h4 class="slider-item-caption-title">Run! Run! Run!</h4>
                                 <p class="slider-item-caption-desc">Your Running Shoes, Right Away.</p><a class="btn btn-lg btn-ghost btn-white" href="#">Shop Now</a>
                             </div>
-                            <img class="slider-item-img-right" src="img/test_slider/3.png" alt="Image Alternative text" title="Image Title" />
+                            <img class="slider-item-img-right" src="<?= base_url('assets/landing/img/test_slider/3.png');?>" alt="Image Alternative text" title="Image Title" />
                         </div>
                     </div>
                 </div>
