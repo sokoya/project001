fbd371e46941db7dd144d3621bc0f6dff2c96e01
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-08-20 10:09:18 +0100

    Helper file created

--- application/controllers/Signup.php
+++ application/controllers/Signup.php
@@ -22,25 +22,19 @@ public function index(){
  * */
 
 function process(){
-        $this->form_validation->set_rules('email_address', 'Email Address','trim|required|xss_clean|valid_email|is_unique[users.email]');
-        $this->form_validation->set_rules('password', 'Password','trim|required|xss_clean|min_length[6]|max_length[12]');
+        $this->form_validation->set_rules('signupemail', 'Email Address','trim|required|xss_clean|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('signuppassword', 'Password','trim|required|xss_clean|min_length[8]|max_length[15]');
+        $this->form_validation->set_rules('signuprepeatpassword', 'Password','trim|required|xss_clean|min_length[8]|max_length[15]|matches[signuppassword]');
 
         if ($this->form_validation->run() == FALSE) {
             $output_array['status'] = 'error';
             $output_array['message'] = 'There was an error with the account creation. Please fix the following <pre>' . $this->form_validation->error_array() . '</pre>';
         }else{
-            $email = $this->input->post('email_address');
-            $post_password = $this->input->post('password');
-            $registration_date = gmdate('Y-m-d H:i:s', time());
-            $hashed_password = $this->user->generate_password_hash($post_password);
-            $registration_key = sha1($registration_date . ';;' . $post_email_address . ';;' . $hashed_password[0]);
-
-            // Develop the array of post data for sending to the model.
             $data = array(
-                'email' => $this->input->post('email_address'),
+                'email' => $this->input->post('signupemail'),
                 'password' => $hashed_password[0],
                 'password_hash' => $hashed_password[1],
-                'registration_date' => $registration_date,
+                'registration_date' => get_now(),
                 'registration_key' => $registration_key
             );
 
@@ -48,4 +42,27 @@ function process(){
         }
     }
 }
+
+
+// User was successfully created and the user needs to verify their account.
+// Send registered an email informing them how to validate their account.
+// $company_name = $this->config->item('company_name');
+// $company_email_address = $this->config->item('company_email');
+
+// $this->load->library('email');
+// $this->email->from($company_email_address, $company_name);
+// $this->email->to($post_email_address);
+// $this->email->subject($company_name.' Registration');
+// $message = 'Welcome to '.$company_name.','."\r\n \r\n";
+// $message .= 'Thank you for joining the '.$company_name.' Team. ';
+// $message .= 'We have listed your registration details below. Make sure you save this email.';
+// $message .= 'To verify this account please click the following link.'."\r\n \r\n";
+// $message .= anchor('register/verify/'.$registration_key, 'Click Here To Activate Your Account', '')."\r\n";
+// $message .= 'Please verfiy your account within 2 hours, otherwise your registration will become invalid and you will have to register again.'."\r\n \r\n";
+// $message .= 'Your email address: '.$post_email_address."\r\n";
+// $message .= 'Your Password: '.$post_password."\r\n \r\n";
+// $message .= 'Enjoy your stay here at '.$company_name.'.'."\r\n \r\n";
+// $message .= 'The '.$company_name.' Team';
+// $this->email->message($message);
+// $this->email->send();
 
---
+++ application/helpers/site_helper.php
@@ -0,0 +1,8 @@
+<?php
+// A Global Site helper function
+
+// Get the present Date and time
+function get_now(){
+	return gmdate('Y-m-d H:i:s', time());
+}
+?>
