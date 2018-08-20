aecd10abea4492bbef55bd8ff50a61188deb67e4
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-08-20 09:47:53 +0100

    Created validation logic and Ajax forms

--- application/views/landing/login.php
+++ application/views/landing/login.php
@@ -1,7 +1,7 @@
 <?php $this->load->view('landing/resources/head_base'); ?>
 <style>
-	
-	.well{
+
+	.well {
 		padding: 5px;
 		font: 12px/20px "Helvetica Neue", Helvetica, Arial, sans-serif;
 	}
@@ -50,7 +50,7 @@
 			</div>
 			<div class="checkbox">
 				<label>
-					<input class="i-check" type="checkbox"/>Remeber Me</label>
+					<input class="i-check" type="checkbox"/>Remember Me</label>
 			</div>
 			<input class="btn btn-primary" type="submit" value="Sign In"/>
 		</form>
@@ -91,7 +91,7 @@
 		</form>
 		<div class="gap gap-small"></div>
 		<ul class="list-inline">
-			<li><a href="#nav-login-dialog" class="popup-text">Already Memeber</a>
+			<li><a href="#nav-login-dialog" class="popup-text">Already Member</a>
 			</li>
 		</ul>
 	</div>
@@ -120,14 +120,14 @@
 						<div class="col-md-6" style="margin-bottom: 40px;">
 							<h3 class="widget-title" style="font-weight: bold; font-size: 20px;padding-bottom: 14px;">
 								Sign in</h3>
-							<form autocorrect="off" autocomplete="off">
+							<form autocorrect="off" id="login-form">
 								<div class="form-group">
 									<label>Email Address*</label>
-									<input class="form-control" type="text"/>
+									<input class="form-control" type="email" name="loginemail"/>
 								</div>
 								<div class="form-group">
 									<label>Password</label>
-									<input class="form-control" type="text"/>
+									<input class="form-control" type="password" name="loginpassword"/>
 								</div>
 								<div class="checkbox">
 									<label>
@@ -137,29 +137,31 @@
 									   value="Sign in"/>
 							</form>
 							<div class="form_end">
-								<a href="<?= base_url('resetpassword');?>">Forgot Your Password?</a>
+								<a href="<?= base_url('resetpassword'); ?>">Forgot Your Password?</a>
 							</div>
 						</div>
 						<hr class="hr-text hidden-lg hidden-md" data-content="OR">
 						<div class="col-md-6">
 							<h3 class="widget-title" style="font-weight: bold; font-size: 20px;padding-bottom: 14px;">
 								Create Account</h3>
-							<form autocorrect="off" autocomplete="off">
+							<form autocorrect="off" autocomplete="off" id="register-form">
 								<div class="form-group">
 									<label>Email Address*</label>
-									<input class="form-control" type="text"/>
+									<input class="form-control" type="text" id="signup-email" name="signupemail"/>
 								</div>
 								<div class="row">
 									<div class="col-md-6">
 										<div class="form-group">
 											<label>Password*</label>
-											<input class="form-control" type="text"/>
+											<input class="form-control" type="password" id="signup-password"
+												   name="signuppassword"/>
 										</div>
 									</div>
 									<div class="col-md-6">
 										<div class="form-group">
 											<label>Repeat Password*</label>
-											<input class="form-control" type="text"/>
+											<input class="form-control" type="password" id="signup-repeat-password"
+												   name="signuprepeatpassword"/>
 										</div>
 									</div>
 								</div>
@@ -167,13 +169,14 @@
 									<div class="checkbox">
 										<label>
 											<input class="i-check" type="checkbox"/>
-											I agree to Carrito <a href="#">Terms & Conditions</a> | <a href="#"> Policy.</a></label>
+											I agree to Carrito <a href="#">Terms & Conditions</a> | <a href="#">
+												Policy.</a></label>
 									</div>
 								</div>
 								<input class="carrito_btn_create col-md-12 col-sm-12 col-xs-12" type="submit"
 									   value="Create Account"/>
 
-								
+
 							</form>
 						</div>
 					</div>
@@ -187,5 +190,97 @@
 
 </div>
 <?php $this->load->view('landing/resources/script'); ?>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.min.js"></script>
+
+
+<script>
+	// Carrito signUp validation
+	$('document').ready(function () {
+		/* validation */
+		$("#register-form").validate({
+			rules:
+				{
+					signupemail: {
+						required: true,
+						email: true
+					},
+					signuppassword: {
+						required: true,
+						minlength: 8,
+						maxlength: 15
+					},
+					signuprepeatpassword: {
+						required: true,
+						equalTo: '#signup-password'
+					},
+				},
+			messages:
+				{
+
+					signuppassword: {
+						required: "Provide a Password",
+						minlength: "Password Needs To Be Minimum of 8 Characters"
+					},
+					signupemail: "Enter a Valid Email",
+					signuprepeatpassword: {
+						required: "Retype Your Password",
+						equalTo: "Password Mismatch! Retype"
+					}
+				},
+			submitHandler: submitForm
+		});
+
+		$("#login-form").validate({
+			rules:
+				{
+					loginemail: {
+						required: true,
+						email: true
+					},
+					loginpassword: {
+						required: true,
+					},
+				},
+			messages:
+				{
+					loginpassword: {
+						required: "Provide a Password",
+						minlength: "Password Needs To Be Minimum of 8 Characters"
+					},
+					loginemail: "Enter a Valid Email",
+				},
+			submitHandler: submitForm
+		});
+		// End Validation
+
+		// Carrito form submit
+		function submitForm() {
+			let data = $("#register-form").serialize();
+
+			$.ajax({
+
+				type: 'POST',
+				url: 'login/process',
+				data: data,
+
+				success: function (data) {
+					if (data === "success") {
+
+						console.log("Success")
+
+					}
+					else if (data === "error") {
+						console.log("Error")
+					}
+					else {
+						console.log("An unknown error occurred")
+					}
+				}
+			});
+			return false;
+		}
+	});
+
+</script>
 </body>
 </html>
