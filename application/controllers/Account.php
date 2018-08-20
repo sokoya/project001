5ab83d9394f6bc5074f2271e6c8255b26e7f7616
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-08-20 10:59:11 +0100

    Created Account page in replace of Customer

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Customer extends CI_Controller{
+class Account extends CI_Controller{
 
     public function __construct(){
         // @todo
--- application/helpers/site_helper.php
+++ application/helpers/site_helper.php
@@ -96,6 +96,15 @@ function cleanit($data) {
 	return $data;
 }
 
+// Clean phone number
+function phoneclean($num) {
+    $num = preg_replace('/\D/', '', $num);
+   	$len = strlen($num);
+   	$accurate = $len - 10;
+   	$realNUM = substr($num,$accurate);
+   	return '0'.$realNUM;
+}
+
 function urlify($string){
     return strtolower(trim(preg_replace('~[^0-9a-z]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), '-'));
 }
