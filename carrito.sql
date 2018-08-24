8aec7528fd7649a81b3471a5c082e6366c4d3238
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-08-24 09:42:56 +0100

    sql

--- carrito.sql
+++ carrito.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 20, 2018 at 07:48 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Host: localhost
+-- Generation Time: Aug 24, 2018 at 08:41 AM
+-- Server version: 5.7.11
+-- PHP Version: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,10 +23,166 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `customers` (
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `root_category_id` int(11) NOT NULL,
+  `inserted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Foreign from root category';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupons`
+--
+
+CREATE TABLE `coupons` (
+  `id` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  `description` text NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `value` float NOT NULL,
+  `multiple` tinyint(1) NOT NULL DEFAULT '0',
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `inserted_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_category` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `product_status_id` int(11) NOT NULL,
+  `regular_price` decimal(10,0) NOT NULL,
+  `discount_price` decimal(10,0) NOT NULL,
+  `inserted_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Product Item Table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_gallery`
+--
+
+CREATE TABLE `product_gallery` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `featured_image` tinyint(1) NOT NULL DEFAULT '0',
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_tags`
+--
+
+CREATE TABLE `product_tags` (
+  `id` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `inserted_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Every products can have a tag based on the admin defined tag';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pr_brand`
+--
+
+CREATE TABLE `pr_brand` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(255) NOT NULL DEFAULT 'No Size Specified'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pr_color`
+--
+
+CREATE TABLE `pr_color` (
+  `color_id` int(11) NOT NULL,
+  `color_name` varchar(255) NOT NULL DEFAULT 'No color specified'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pr_size`
+--
+
+CREATE TABLE `pr_size` (
+  `size_id` int(11) NOT NULL,
+  `size_name` varchar(255) NOT NULL DEFAULT 'No Size Specified'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `root_category`
+--
+
+CREATE TABLE `root_category` (
+  `root_category_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `inserted_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_category`
+--
+
+CREATE TABLE `sub_category` (
+  `sub_category_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Sub Category from Category Table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `inserted_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `first_name` varchar(100) NOT NULL,
@@ -43,24 +197,87 @@ CREATE TABLE `customers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `customers`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `customers` (`id`, `email`, `first_name`, `last_name`, `password`, `salt`, `ip`, `date_registered`, `last_login`, `recovery_code`, `account_status`) VALUES
-(1, 'ph@gmail.com', 'hhshsh', '0', '8e5f564b3b8b4b8eb5672757e65bbede001ddcc16c86378194b6ad1d85af31d0', '.LE3pVWB%&O=bKR4uDrp5JehiGXq5?)k*Mm(2TPr*Y,n_wUN1<', '::1', '2018-08-20 17:26:27', '2018-08-20 17:26:27', '', ''),
-(2, 'second@gmail.com', 'Second Account', 'Second Account', '99fae15026c9ea229a23d5675a014ced0a06c39c4c28b1ab90b802525e9ca8d2', 'Xb3gGZYk4(ZDfsbXm$Zi%2fuUe!n;$zGqxpC3rM_v0:w^ehxL;', '::1', '2018-08-20 17:33:45', '2018-08-20 17:33:45', '', ''),
-(3, 'third@gmail.com', 'Third Account', 'Third Account', '7676aeb054c92bd154f397f02a07ec758b2498cdeb45d8b6562ace892d4b64ab', '%,:2N.+H=lV^^Y?Rl9c9y6HMzkza!0fxQPU==D=ZD|7E:!GSGo', '::1', '2018-08-20 17:35:19', '2018-08-20 17:35:19', '', ''),
-(4, '4th@gmail.com', 'Fouth Acct', 'Fouth Acct', 'a7ee3aff7c08968b5ddc41263b6ebb419a2c8feeaf66da3b0b64399451d6c457', 'Nt2;6-auk2x>=L5GPTx:7Y%UiBZE61+cPod50&Taw)8lWhjnYr', '::1', '2018-08-20 17:37:48', '2018-08-20 17:37:48', '', ''),
-(5, 'test@test.com', 'test', 'test', '7048a0528dd1afe0bc46c1314cb6cef5b2225818f90dc01af3ea5f8bda1f5b8a', '<RBL=BP|sO3(c)kCqmelP.-i@RpM)%;*g:XSUcHsHZRIYqk#&L', '::1', '2018-08-20 17:45:53', '2018-08-20 17:45:53', '', '');
+INSERT INTO `users` (`id`, `email`, `first_name`, `last_name`, `password`, `salt`, `ip`, `date_registered`, `last_login`, `recovery_code`, `account_status`) VALUES
+(1, 'bisi@gmail.com', 'Sokoya', 'Philip', '9c5326f0503d10e655eb6b51c2672c4a5c1bee11f033fda55b76110ad287802c', '8V8mAbNg|N)OT13<U|Q|h%&bYz1>ILEdhF1XsiBFpRN$_9UD#c', '::1', '2018-08-23 16:21:31', '2018-08-23 16:21:31', '', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `customers`
+-- Indexes for table `category`
 --
-ALTER TABLE `customers`
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `coupons`
+--
+ALTER TABLE `coupons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sku` (`sku`);
+
+--
+-- Indexes for table `product_gallery`
+--
+ALTER TABLE `product_gallery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_tags`
+--
+ALTER TABLE `product_tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pr_brand`
+--
+ALTER TABLE `pr_brand`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `pr_color`
+--
+ALTER TABLE `pr_color`
+  ADD PRIMARY KEY (`color_id`);
+
+--
+-- Indexes for table `pr_size`
+--
+ALTER TABLE `pr_size`
+  ADD PRIMARY KEY (`size_id`);
+
+--
+-- Indexes for table `root_category`
+--
+ALTER TABLE `root_category`
+  ADD PRIMARY KEY (`root_category_id`);
+
+--
+-- Indexes for table `sub_category`
+--
+ALTER TABLE `sub_category`
+  ADD PRIMARY KEY (`sub_category_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -68,12 +285,65 @@ ALTER TABLE `customers`
 --
 
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT for table `category`
 --
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `coupons`
+--
+ALTER TABLE `coupons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_gallery`
+--
+ALTER TABLE `product_gallery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_tags`
+--
+ALTER TABLE `product_tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pr_brand`
+--
+ALTER TABLE `pr_brand`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pr_color`
+--
+ALTER TABLE `pr_color`
+  MODIFY `color_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pr_size`
+--
+ALTER TABLE `pr_size`
+  MODIFY `size_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `root_category`
+--
+ALTER TABLE `root_category`
+  MODIFY `root_category_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sub_category`
+--
+ALTER TABLE `sub_category`
+  MODIFY `sub_category_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
