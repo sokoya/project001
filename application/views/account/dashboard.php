12c2b1fc6f963f1725328252d1cc11fc326811b7
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-08-31 17:13:21 +0100

    Updated the buyers page

--- application/views/account/dashboard.php
+++ application/views/account/dashboard.php
@@ -7,14 +7,15 @@
 
 	<?php $this->load->view('landing/resources/head_menu') ?>
 
-	<div class="container">
-		<div class="gap gap-small"></div>
-		<div class="row row-col-border" data-gutter="60"">
-			<div class="col-md-4">
-				<!-- Menu Section -->
-				<ul>
+	<div class="container carrito-dashboard-cover">
+
+		<div class="col-md-4">
+			<div class="hidden-xs hidden-md">
+				<h3 class="carrito-sidebar-header">My Account</h3>
+				<hr class="carrito-sidebar-line"/>
+				<ul class="carrito-sidebar">
 					<li>
-						<h4><a href="<?= base_url('account'); ?>">Overview</a></h4>
+						<h4 class="carrito-sidebar-active"><a href="<?= base_url('account'); ?>">Overview</a></h4>
 					</li>
 					<li>
 						<h4><a href="<?= base_url('account/information'); ?>">Information</a></h4>
@@ -32,16 +33,86 @@
 						<h4><a href="<?= base_url('account/settings'); ?>">Account Settings</a></h4>
 					</li>
 				</ul>
-				<div class="gap gap-small"></div>
 			</div>
-			<div class="col-md-8">
-				<!-- Control panel -->
-				<p>Welcome here too</p>
+
+
+		</div>
+		<div class="col-md-8">
+			<ul class="carrito-mobile-dropdown hidden-lg">
+				<li class="dropdown carrito-custom-dropdown"><a href="#" class="carrito-sidebar-header-r cx">Overview<i
+							class="drop-caret custom-drop"
+							data-toggle="dropdown"></i></a>
+					<ul class="dropdown-menu list-group custom-mobile-navbar">
+						<li class="list-group-item">
+							<h4><a href="<?= base_url('account'); ?>">Overview</a></h4>
+						</li>
+						<li class="list-group-item">
+							<h4><a href="<?= base_url('account/information'); ?>">Information</a></h4>
+						</li>
+						<li class="list-group-item">
+							<h4><a href="<?= base_url('account/my_orders'); ?>">My Orders</a></h4>
+						</li>
+						<li class="list-group-item">
+							<h4><a href="<?= base_url('account/reviews'); ?>">My Reviews & Ratings</a></h4>
+						</li>
+						<li class="list-group-item">
+							<h4><a href="<?= base_url('account/saved'); ?>">My Saved Items</a></h4>
+						</li>
+						<li class="list-group-item">
+							<h4><a href="<?= base_url('account/settings'); ?>">Account Settings</a></h4>
+						</li>
+					</ul>
+
+				</li>
+			</ul>
+			<h3 class="carrito-sidebar-header-r hidden-sm hidden-md hidden-xs">Overview</h3>
+			<hr class="carrito-sidebar-line-r"/>
+			<div class="alert alert-warning">
+				<i class="fa fa-warning"></i> Due to severe wildfire conditions in Calabar, deliveries To and From
+				several area in the state may arrive latter than expected. To view the most up to date status for your
+				order, please go to the Orders page
 			</div>
+			<p class="carrito-dashboard-welcome-text">Hello Chidi Jeffrey,</p>
+			<p class="carrito-dashboard-welcome-text-body">From your My Account Dashboard you have the ability to view a
+				snapshot of your recent account activity and update your account information. Select a link below to
+				view or edit information.</p>
+			<div class="row ">
+				<div class="col-md-4">
+					<div class="carrito-dashboard-card">
+						<h4>My Orders</h4>
+						<hr/>
+						<p>
+							Check order status make a return or write a review
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="carrito-dashboard-card">
+						<h4>Account Settings</h4>
+						<hr/>
+						<p>
+							Change personal information, update address book and add authorized users
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="carrito-dashboard-card">
+						<h4>My Saved Items</h4>
+						<hr/>
+						<p>
+							Manage your saved items all from one location
+						</p>
+					</div>
+
+				</div>
+
+			</div>
+
 		</div>
 	</div>
-	<div class="gap gap-small"></div>
-	<?php $this->load->view('landing/resources/footer'); ?>
+</div>
+<div class="gap gap-small"></div>
+<?php $this->load->view('landing/resources/footer'); ?>
 </div>
 <?php $this->load->view('landing/resources/script'); ?>
 </body>
