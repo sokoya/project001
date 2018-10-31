be439af82ab6e550a48bc494f9c7cdb328f96182
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-10-31 19:49:22 +0100

    update

--- application/views/landing/product.php
+++ application/views/landing/product.php
@@ -200,9 +200,10 @@
 												<select class="product-page-option-select variation-select"
 														name="colour">
 													<?php foreach ($variations as $variation): ?>
-														<option title="<?= $variation->variation;?>" data-id="<?= $variation->id ?>"
+														<option title="<?= $variation->variation; ?>"
+																data-id="<?= $variation->id ?>"
 																value="<?= trim($variation->variation); ?>" <?php if ($variation->quantity == 0) echo 'disabled'; ?> >
-																
+
 															<?= ellipsize(trim($variation->variation), 8); ?>
 														</option>
 													<?php endforeach; ?>
@@ -228,7 +229,8 @@
 													<button type="button"
 															class="product-page-qty product-page-qty-minus">-
 													</button>
-													<input data-range="<?= $variation->quantity; ?>" name="quantity" id="quan"
+													<input data-range="<?= $variation->quantity; ?>" name="quantity"
+														   id="quan"
 														   class="product-page-qty product-page-qty-input quantity"
 														   type="text"
 														   value="1" disabled/>
@@ -310,7 +312,8 @@
 					</li>
 					<li><a href="#full-spec" data-toggle="tab"><i class="fa fa-cogs nav-tab-icon"></i>Full Specs</a>
 					</li>
-					<li><a href="#review" data-toggle="tab"><i class="fa fa-star nav-tab-icon"></i>Rating and Reviews</a>
+					<li><a href="#review" data-toggle="tab"><i class="fa fa-star nav-tab-icon"></i>Rating and
+							Reviews</a>
 					</li>
 				</ul>
 				<div class="tab-content">
@@ -424,36 +427,44 @@
 									</li>
 									<li class="count">4.9</li>
 								</ul>
-								<small>{{ Number of reviews - 238 customer reviews }} </small>
+								<small>{{ Number of reviews - 238 customer reviews }}</small>
 								<p><strong>98%</strong> of reviewers would recommend this product</p>
 								<a class="btn btn-primary" href="#">Write a Review</a>
 							</div>
 							<div class="col-md-5">
 								<form id="review">
-									<ul class="product-page-product-rating product-rating-big">
-										<li class=""><i class="fa fa-star"></i>
-										</li>
-										<li class=""><i class="fa fa-star"></i>
-										</li>
-										<li class=""><i class="fa fa-star"></i>
-										</li>
-										<li class=""><i class="fa fa-star"></i>
-										</li>
-										<li class=""><i class="fa fa-star"></i>
-										</li>
-										<li class="rating-text"></li>
-									</ul>
+									<div class='starrr' id='star1'></div>
+<!--									<div>&nbsp;-->
+<!--										<span class='your-choice-was' style='display: none;'>-->
+<!--        									Your rating was <span class='choice'></span>.-->
+<!--      									</span>-->
+<!--									</div>-->
+<!--									<ul class="product-page-product-rating product-rating-big">-->
+<!--										<li class=""><i class="fa fa-star"></i>-->
+<!--										</li>-->
+<!--										<li class=""><i class="fa fa-star"></i>-->
+<!--										</li>-->
+<!--										<li class=""><i class="fa fa-star"></i>-->
+<!--										</li>-->
+<!--										<li class=""><i class="fa fa-star"></i>-->
+<!--										</li>-->
+<!--										<li class=""><i class="fa fa-star"></i>-->
+<!--										</li>-->
+<!--										<li class="rating-text"></li>-->
+<!--									</ul>-->
 									<div class="row">
 										<div class="col-md-6">
 											<div class="form-group">
 												<label>Title of review*</label>
-												<input type="text" name="title" placeholder="Title of the review" class="form-control">
+												<input type="text" name="title" placeholder="Title of the review"
+													   class="form-control">
 											</div>
 										</div>
 										<div class="col-md-6">
 											<div class="form-group">
 												<label>Display name*</label>
-												<input type="text" name="display_name" placeholder="Display name" class="form-control">
+												<input type="text" name="display_name" placeholder="Display name"
+													   class="form-control">
 											</div>
 										</div>
 									</div>
@@ -527,7 +538,8 @@
 								</ul>
 								<p class="product-review-meta">by {{Display name}} on 08/14/2015</p>
 								<p class="product-review-body">{{ Review Content }}</p>
-								<p class="text-success"><strong><i class="fa fa-check"></i> {{I would recommend it to a friend}}</strong>
+								<p class="text-success"><strong><i class="fa fa-check"></i> {{I would recommend it to a
+										friend}}</strong>
 								</p>
 							</div>
 						</article>
