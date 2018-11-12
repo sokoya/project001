0bd7c689d6ae17574a67e617290610395775aba8
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-11-12 22:56:36 +0100

    updated quickview

--- assets/landing/js/quick-view.js
+++ assets/landing/js/quick-view.js
@@ -56,31 +56,6 @@ function get_view() {
 		qv_location = $(p_id);
 	}
 
-	qv_location.after(
-		`<div class="col-md-12 test-div q_view clearfix">
-			<div class="row">
-			<div class="col-md-4">
-				<img src="${img_src}" class="q_pr_img" alt="${title}" title="${title}">
-			</div>
-			<div class="col-md-8">
-			<span class="close_qv"><i class="fa fa-times-circle-o" aria-hidden="true"></i></span>
-				<h1 class="q_pr_title">${title}</h1>
-				<div class="q_view_high">
-					<div class="q_skeleton_highlight"></div>
-				</div>
-				<div class="row">
-				<div class="col-md-6">
-					<button class="btn btn-block btn-primary add-to-cart c-hover"><i class="fa fa-shopping-cart"></i> Add To Cart</button>
-				</div>
-				<div class="col-md-6">
-					<button class="btn btn-block btn-default fav c-hover"><i class="fa fa-star-o"></i> Wishlist</button>
-				</div>
-				</div>
-
-			</div>	
-			</div>
-		</div>`
-	);
 
 	$('.close_qv').on('click', function () {
 		$('.test-div').remove();
@@ -91,12 +66,87 @@ function get_view() {
 		method: 'POST',
 		data: {product_id: pr_id},
 		success: function (response) {
-			$.each(JSON.parse(response), function (key, value) {
-				$('.q_view_high').html(`
-					<p>${value.highlights}</p>
-					`)
-			});
-		},
+			let quick = JSON.parse(response);
+			qv_location.after(
+				`<div class="col-md-12 test-div q_view clearfix">
+			<div class="row">
+			<div class="col-md-4">
+				<img src="${img_src}" class="q_pr_img" alt="${title}" title="${title}">
+			</div>
+			<div class="col-md-8">
+			<span class="close_qv"><i class="fa fa-times-circle-o" aria-hidden="true"></i></span>
+				<h1 class="q_pr_title">${title}</h1>
+				<div class="q_view_high">
+					<div class="q_skeleton_highlight">${quick.description}</div>
+				</div>
+				<div class="row" style="margin-top: 20px; margin-bottom: 10px">
+									<div class="col-md-6">
+					<h5 class="custom-product-page-option-title">Quantity:</h5>
+						<ul>
+							<li class="product-page-qty-item">
+								<button type="button"
+										class="product-page-qty product-page-qty-minus">-
+								</button>
+								<input data-range="10" name="quantity"
+									   id="quan"
+									   class="product-page-qty product-page-qty-input quantity"
+									   type="text"
+									   value="1"/>
+								<button type="button"
+										class="product-page-qty product-page-qty-plus">+
+								</button>
+							</li>
+						</ul>
+					</div>
+
+					<div class="col-md-6" id="variation_container">
+				
+					</div>
+				
+				</div>
+				<div class="row">
+				<div class="col-md-6">
+					<button class="btn btn-block btn-primary add-to-cart c-hover"><i class="fa fa-shopping-cart"></i> Add To Cart</button>
+				</div>
+				<div class="col-md-6">
+					<button class="btn btn-block btn-default fav c-hover"><i class="fa fa-star-o"></i> Wishlist</button>
+				</div>
+				</div>
+			</div>	
+			</div>
+		</div>`
+			);
+			if (quick.variation.length > 1) {
+
+				$('#variation_container').append(
+					`<h5 class="custom-product-page-option-title">Variation:</h5>
+<select class="product-page-option-select variation-select" id="variation_select" name="variation">
+					</select>`
+				);
+
+				$.each(quick.variation, function (key, value) {
+					$('#variation_select').append($('<option>', {
+						value: value.variation_name,
+						text: value.variation_name
+					}));
+					// `<select class="product-page-option-select variation-select"	name="variation">
+					// 	<option title="${value.variation_name}"
+					// 			data-id="${value.pid}"
+					// 			value="${value.variation_name}">
+					// 		${value.variation_name}
+					// 	</option>
+					// </select>`;
+
+					// console.log(value.variation_name)
+				});
+			}
+			// $.each(JSON.parse(response), function (key, value) {
+			// 	$('.q_view_high').html(`
+			// 		<p>${value.description}</p>
+			// 		`)
+			// });
+		}
+		,
 		error: () => {
 			console.log('An error occurred')
 		}
