37308c06af79dcc94087d9dc2a296b98cdaf3507
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-11-13 23:03:54 +0100

    Update application/models/Seller_model.php

--- application/models/Seller_model.php
+++ application/models/Seller_model.php
@@ -333,7 +333,7 @@ Class Seller_model extends CI_Model{
         }else{
             $this->db->where('seller_id', $sid);
             $this->db->where('id', $id);
-            if( $this->db->update( 'sellers_notification_message', array('is_read', 1) ) ){
+            if( $this->db->update( 'sellers_notification_message', array('is_read' => 1) ) ){
                 $this->db->where('seller_id', $sid);
                 $this->db->where('id', $id);
                 return $this->db->get('sellers_notification_message')->row_array();
