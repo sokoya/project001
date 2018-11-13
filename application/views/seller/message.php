921ed8e2d24426d4e4a209d228c516b23796a3c9
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2018-11-13 11:59:44 +0100

    Update application/views/seller/message.php

--- application/views/seller/message.php
+++ application/views/seller/message.php
@@ -66,8 +66,6 @@
                                                     <li><a href="javascript:;" id="demo-select-read-list">Read</a></li>
                                                     <li><a href="javascript:;" id="demo-select-unread-list">Unread</a>
                                                     </li>
-                                                    <li><a href="javascript:;" id="demo-select-starred-list">Starred</a>
-                                                    </li>
                                                 </ul>
                                             </div>
 
@@ -85,13 +83,10 @@
                                                     More <i class="dropdown-caret"></i>
                                                 </button>
                                                 <ul class="dropdown-menu">
-                                                    <li><a href="javascript:;">Mark as read</a></li>
-                                                    <li><a href="javascript:;">Mark as unread</a></li>
+                                                    <li><a href="javascript:;"><i class="demo-pli-mail-send"></i> Mark as read</a></li>
+                                                    <li><a href="javascript:;"><i class="demo-pli-mail-unread"></i> Mark as unread</a></li>
                                                     <li class="divider"></li>
-                                                    <li><a href="javascript:;">Star</a></li>
-                                                    <li><a href="javascript:;">Clear Star</a></li>
-                                                    <li class="divider"></li>
-                                                    <li><a href="javascript:;">Delete</a></li>
+                                                    <li><a href="javascript:;"><i class="demo-pli-recycling"></i> Delete</a></li>
                                                 </ul>
                                             </div>
                                         </div>
@@ -122,11 +117,8 @@
                                                 <input id="email-list-1" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-1"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Admin</a></div>
                                             <div class="mail-time">05:55 PM</div>
-                                            <div class="mail-attach-icon"></div>
                                             <div class="mail-subject">
                                                 <a href="javascript:;">Track your order </a>
                                             </div>
@@ -138,11 +130,8 @@
                                                 <input id="email-list-2" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-2"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Onitshamarket.com</a></div>
                                             <div class="mail-time">10:45 AM</div>
-                                            <div class="mail-attach-icon"></div>
                                             <div class="mail-subject">
                                                 <a href="javascript:;">Tracking Your Order - Shoes Store Online</a>
                                             </div>
@@ -154,11 +143,8 @@
                                                 <input id="email-list-3" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-3"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Admin</a></div>
                                             <div class="mail-time">07:18 AM</div>
-                                            <div class="mail-attach-icon"><i class="demo-psi-paperclip"></i></div>
                                             <div class="mail-subject">
                                                 <a href="javascript:;">Reset your account password</a>
                                             </div>
@@ -170,11 +156,8 @@
                                                 <input id="email-list-4" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-4"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Admin</a></div>
                                             <div class="mail-time">01:51 PM</div>
-                                            <div class="mail-attach-icon"></div>
                                             <div class="mail-subject">
                                                 <a href="javascript:;">
                                                     Regarding to your website issues.
@@ -188,11 +171,8 @@
                                                 <input id="email-list-5" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-5"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Admin</a></div>
                                             <div class="mail-time">Yesterday</div>
-                                            <div class="mail-attach-icon"></div>
                                             <div class="mail-subject">
                                                 <a href="javascript:;">Hi John! How are you?</a>
                                             </div>
@@ -204,11 +184,8 @@
                                                 <input id="email-list-6" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-6"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Onitshamarket.com</a></div>
                                             <div class="mail-time">Yesterday</div>
-                                            <div class="mail-attach-icon"></div>
                                             <div class="mail-subject">
                                                 <a href="javascript:;">
                                                     Repair Status Unregistered User
@@ -222,11 +199,8 @@
                                                 <input id="email-list-7" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-7"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Admin</a></div>
                                             <div class="mail-time">Oct 10</div>
-                                            <div class="mail-attach-icon"><i class="demo-psi-paperclip"></i></div>
                                             <div class="mail-subject"><a href="javascript:;">Bugs in your system.</a>
                                             </div>
                                         </li>
@@ -237,11 +211,8 @@
                                                 <input id="email-list-8" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-8"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Admin</a></div>
                                             <div class="mail-time">Oct 10</div>
-                                            <div class="mail-attach-icon"></div>
                                             <div class="mail-subject"><a href="javascript:;">This is an example if there
                                                     is a really really long text. Lorem ipsum dolor sit amet,
                                                     consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -254,11 +225,8 @@
                                                 <input id="email-list-9" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-9"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Onitshamarket.com</a></div>
                                             <div class="mail-time">Oct 9</div>
-                                            <div class="mail-attach-icon"><i class="demo-psi-paperclip"></i></div>
                                             <div class="mail-subject"><a href="javascript:;">Tracking Your Order - Shoes
                                                     Store Online</a></div>
                                         </li>
@@ -268,11 +236,8 @@
                                                 <input id="email-list-14" class="magic-checkbox" type="checkbox">
                                                 <label for="email-list-14"></label>
                                             </div>
-                                            <div class="mail-star"><a href="javascript:;"><i class="demo-psi-star"></i></a>
-                                            </div>
                                             <div class="mail-from"><a href="javascript:;">Onitshamarket.com</a></div>
                                             <div class="mail-time">Oct 3</div>
-                                            <div class="mail-attach-icon"><i class="demo-psi-paperclip"></i></div>
                                             <div class="mail-subject"><a href="javascript:;">Bugs in your system.</a>
                                             </div>
                                         </li>
@@ -344,10 +309,10 @@
                                     <div class="col-sm-5 toolbar-right">
                                         <!--Reply & forward buttons-->
                                         <div class="btn-group btn-group">
-                                            <a class="btn btn-default" href="#">
+                                            <a class="btn btn-default" href="javascript:;">
                                                 <i class="demo-psi-left-4"></i>
                                             </a>
-                                            <a class="btn btn-default" href="#">
+                                            <a class="btn btn-default" href="javascript:;">
                                                 <i class="demo-psi-right-4"></i>
                                             </a>
                                         </div>
@@ -356,34 +321,55 @@
 
                                 <!--Message-->
                                 <!--===================================================-->
-                                <div class="mail-message">
-                                    Hey John,<br/><br/>
-                                    <blockquote style="font-size:14px;text-align:justify;">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                        culpa qui officia deserunt mollit anim id est laborum.
-                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                                        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                                        veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                                        voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                                        consequuntur
-                                        magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-                                        est,
-                                        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
-                                        numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-                                        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-                                        suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel
-                                        eum
-                                        iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-                                        consequatur.
-                                    </blockquote>
-                                    <div class="pull-right">
-                                        <br><br> Regards,
-                                        <br><br>
-                                        <strong>Admin</strong><br>admin@onitshamarket.com<br>
+                                <div class="nano has-scrollbar" style="height: 350px;">
+                                    <div class="nano-content" tabindex="0" style="right: -17px;">
+                                        <div class="mail-message">
+                                            Hey John,<br/><br/>
+                                            <blockquote style="font-size:14px;text-align:justify;">
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                tempor
+                                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                                quis
+                                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                                consequat.
+                                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                                                dolore eu
+                                                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                                sunt in
+                                                culpa qui officia deserunt mollit anim id est laborum.
+                                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                                                accusantium
+                                                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                                                inventore
+                                                veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                                                enim ipsam
+                                                voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                                                consequuntur
+                                                magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
+                                                quisquam
+                                                est,
+                                                qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+                                                quia non
+                                                numquam eius modi tempora incidunt ut labore et dolore magnam aliquam
+                                                quaerat
+                                                voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                                                corporis
+                                                suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
+                                                autem vel
+                                                eum
+                                                iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+                                                consequatur.
+                                            </blockquote>
+                                            <div class="pull-right">
+                                                <br><br> Regards,
+                                                <br><br>
+                                                <strong>Admin</strong><br>admin@onitshamarket.com<br>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="nano-pane">
+                                        <div class="nano-slider"
+                                             style="height: 20px; transform: translate(0px, 0px);"></div>
                                     </div>
                                 </div>
 
