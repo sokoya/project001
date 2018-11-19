fc2309cb76b843069b3c28aad47986ae25ceb934
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-11-19 12:48:15 +0100

    updates

--- application/views/landing/mobile-category.php
+++ application/views/landing/mobile-category.php
@@ -1,8 +1,78 @@
 <?php $this->load->view('landing/resources/head_base'); ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css"/>
-</head>
-<body>
+<style type="text/css">
+	.feature-attribute:hover {
+		cursor: pointer;
+	}
 
+	.carrito-checkbox {
+		display: block;
+		position: relative;
+		padding-left: 25px;
+		margin-bottom: 10px;
+		cursor: pointer;
+		font-size: 14px;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+
+	.carrito-checkbox input {
+		position: absolute;
+		opacity: 0;
+		cursor: pointer;
+	}
+
+	.checkmark {
+		cursor: pointer;
+		position: absolute;
+		top: 0;
+		left: 0;
+		padding: 3px;
+		margin-top: 2px;
+		height: 15px;
+		width: 15px;
+		border: solid 1px #74c68366;;
+		background-color: #fff;
+	}
+
+	.carrito-checkbox:hover input ~ .checkmark {
+		border-color: #74c68366;
+	}
+
+	.carrito-checkbox input:checked ~ .checkmark {
+		background-color: #74c683;
+	}
+
+	;
+
+	.checkmark:after {
+		content: "";
+		position: absolute;
+		display: none;
+	}
+
+	/* Show the checkmark when checked */
+	.carrito-checkbox input:checked ~ .checkmark:after {
+		display: block;
+		color: white;
+	}
+
+	/* Style the checkmark/indicator */
+	.carrito-checkbox .checkmark:after {
+		left: 9px;
+		top: 5px;
+		width: 5px;
+		height: 10px;
+		border: solid white;
+		border-width: 0 3px 3px 0;
+		-webkit-transform: rotate(45deg);
+		-ms-transform: rotate(45deg);
+		transform: rotate(45deg);
+	}
+</style>
+</head>
 <body>
 <div class="global-wrapper clearfix" id="global-wrapper">
 	<?php $this->load->view('landing/resources/head_img') ?>
@@ -14,13 +84,15 @@
 			<ol class="breadcrumb page-breadcrumb">
 				<li><a href="<?= base_url(); ?>">Home</a>
 				</li>
-				<li class="active"><?= ucwords($searched); ?>
+				<li class="active"><?= ucwords($category_detail->name); ?>
 				</li>
 			</ol>
 			<div class="category-selections clearfix">
 				<button class="btn btn-custom-primary">Newest First</button>
 				<button class="btn btn-custom-primary">Best Sellers</button>
-				<button class="btn btn-custom-primary"><i class="fa fa-filter" aria-hidden="true"></i> Filter</button>
+				<button class="btn btn-custom-primary filter-btn"><i class="fa fa-filter"
+																	 aria-hidden="true"></i> Filter
+				</button>
 			</div>
 		</header>
 		<div class="row">
@@ -186,6 +258,55 @@
                     </div>
                 </aside>
             </div> -->
+			<div id="ont_filter" class="filterbar">
+				<div class="w-bg top_menu">
+					<a href="javascript:void(0)" class="update_fil" style="float: right">Update Filter</a>
+					<p><span class="filter_close_btn"><i class="fa fa-arrow-left" aria-hidden="true"></i></span> &nbsp;Filter
+					</p>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading filter-head filter-first">Price</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-4" style="padding-right: 3px">
+								<input type="text" class="form-control price_min" placeholder="&#8358; Min">
+							</div>
+							<div class="col-xs-4" style="padding-left:  3px !important; padding-right: 3px;">
+								<input type="text" class="form-control price_max" placeholder="&#8358; Max">
+							</div>
+							<div class="col-xs-4">
+								<input type="submit" class="price-submit" value="Go">
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<?php if ($features) : ?>
+					<?php $x = 1;
+					foreach ($features as $feature => $feature_value) : ?>
+						<div class="panel panel-default">
+							<div
+								class="panel-heading filter-head"><?= preg_replace("/[^A-Za-z 0-9]/", ' ', $feature); ?></div>
+							<div class="panel-body">
+								<?php foreach ($feature_value as $key => $value) : ?>
+									<div class="carrito-checkbox">
+										<label class="list-label">
+											<input class="filter" type="checkbox" name="filterset"
+												   data-type="<?= trim($feature); ?>"
+												   data-value="<?= trim(preg_replace("/[^A-Za-z0-9-]/", '_', $value)) ?>"/><?= $value; ?>
+											<span class="checkmark"></span>
+										</label>
+									</div>
+									<hr class="panel-line"/>
+								<?php endforeach; ?>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				<?php endif; ?>
+
+
+			</div>
 			<div class="col-md-9">
 				<div class="row row-sm-gap" data-gutter="10">
 					<?php foreach ($products as $product) : ?>
@@ -195,7 +316,8 @@
 								<div class="product-img-wrap">
 									<img class="product-img"
 										 src="<?= base_url('data/products/' . $product->id . '/' . $product->image_name); ?>"
-										 alt="<?= $product->product_name; ?>" title="<?= $product->product_name; ?>"/>
+										 alt="<?= $product->product_name; ?>"
+										 title="<?= $product->product_name; ?>"/>
 								</div>
 								<a class="product-link"
 								   href="<?= base_url(urlify($product->product_name, $product->id)); ?>"></a>
@@ -231,7 +353,8 @@
 										?>
 									</ul>
 									<h5 class="product-caption-title"><?= word_limiter(ucwords($product->product_name), 14, '...'); ?></h5>
-									<h4 class="product-caption-title"><strong>Seller: </strong><?= ucfirst($product->first_name); ?></h4>
+									<h4 class="product-caption-title">
+										<strong>Seller: </strong><?= ucfirst($product->first_name); ?></h4>
 									<div class="product-caption-price">
 										<?php if (!empty($product->discount_price)) : ?> <span
 											class="product-caption-price-old"><?= ngn($product->sale_price); ?></span>
@@ -239,7 +362,8 @@
 												class="product-caption-price-new"><?= ngn($product->discount_price); ?></span>
 										<?php else : ?>
 
-											<span class="product-caption-price-new"><?= ngn($product->sale_price); ?></span>
+											<span
+												class="product-caption-price-new"><?= ngn($product->sale_price); ?></span>
 										<?php endif; ?>
 									</div>
 								</div>
@@ -262,10 +386,8 @@
 <script type="text/javascript"
 		src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.plugins.min.js"></script>
 <script>
-	let base_url = "<?= base_url(); ?>";
 	let current_url = "<?= current_url()?>";
 </script>
-<script src="<?= base_url('assets/landing/js/quick-view.js'); ?>"></script>
 <script src="<?= base_url('assets/landing/js/search.js'); ?>"></script>
 <script>
 	$("#price-slider").ionRangeSlider({
@@ -346,6 +468,25 @@
 			}
 		});
 	});
+
+	$('.filter_close_btn').on('click', function () {
+		filterBarClose();
+	});
+	filterBarOpen();
+	$('.filter-btn').on('click', function () {
+		filterBarOpen();
+	});
+
+	// Side bar
+	function filterBarOpen() {
+		// $('#ont_filter').css({'width': '100%'});
+		$('#ont_filter').show();
+	}
+
+	function filterBarClose() {
+		// $('#ont_filter').css({'width': 0});
+		$('#ont_filter').fadeOut();
+	}
 </script>
 </body>
 </html>
