e78f1d71ec290c66123c5f6361c0cfb6490b7186
Adedoyin Michael Adekoniye <michaeladekoniye@gmail.com>
2018-11-21 16:27:28 +0100

    Update dashboard.php

--- application/views/seller/dashboard.php
+++ application/views/seller/dashboard.php
@@ -419,9 +419,6 @@
                                                     <th>Invoice</th>
                                                     <th>User</th>
                                                     <th>Order date</th>
-                                                    <th>Amount</th>
-                                                    <th class="text-center">Status</th>
-                                                    <th class="text-center">Tracking Number</th>
                                                 </tr>
                                                 </thead>
                                                 <tbody>
@@ -429,51 +426,26 @@
                                                     <td><a href="#" class="btn-link"> Order #53431</a></td>
                                                     <td>Steve N. Horton</td>
                                                     <td><span class="text-muted">Oct 22, 2014</span></td>
-                                                    <td>&#8358; 45.00</td>
-                                                    <td class="text-center">
-                                                        <div class="label label-table label-success">Paid</div>
-                                                    </td>
-                                                    <td class="text-center">-</td>
                                                 </tr>
                                                 <tr>
                                                     <td><a href="#" class="btn-link"> Order #53434</a></td>
                                                     <td>Teresa L. Doe</td>
                                                     <td><span class="text-muted">Oct 15, 2014</span></td>
-                                                    <td>&#8358; 77.99</td>
-                                                    <td class="text-center">
-                                                        <div class="label label-table label-info">Shipped</div>
-                                                    </td>
-                                                    <td class="text-center">CGX0089734574</td>
                                                 </tr>
                                                 <tr>
                                                     <td><a href="#" class="btn-link"> Order #53435</a></td>
                                                     <td>Teresa L. Doe</td>
                                                     <td><span class="text-muted">Oct 12, 2014</span></td>
-                                                    <td>&#8358; 18.00</td>
-                                                    <td class="text-center">
-                                                        <div class="label label-table label-success">Paid</div>
-                                                    </td>
-                                                    <td class="text-center">-</td>
                                                 </tr>
                                                 <tr>
                                                     <td><a href="#" class="btn-link">Order #53437</a></td>
                                                     <td>Charles S Boyle</td>
                                                     <td><span class="text-muted">Oct 17, 2014</span></td>
-                                                    <td>&#8358; 658.00</td>
-                                                    <td class="text-center">
-                                                        <div class="label label-table label-danger">Refunded</div>
-                                                    </td>
-                                                    <td class="text-center">-</td>
                                                 </tr>
                                                 <tr>
                                                     <td><a href="#" class="btn-link">Order #536584</a></td>
                                                     <td>Scott S. Calabrese</td>
                                                     <td><span class="text-muted">Oct 19, 2014</span></td>
-                                                    <td>&#8358; 45.58</td>
-                                                    <td class="text-center">
-                                                        <div class="label label-table label-warning">Unpaid</div>
-                                                    </td>
-                                                    <td class="text-center">-</td>
                                                 </tr>
                                                 </tbody>
                                             </table>
