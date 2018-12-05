cdf728dc9cb10b1585883627b90d1016d7f1dff6
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-12-05 14:08:57 +0100

    Update script.php

--- application/views/landing/resources/script.php
+++ application/views/landing/resources/script.php
@@ -1,6 +1,6 @@
 <script src="<?= base_url('assets/landing/js/jquery.js'); ?>"></script>
 <script src="<?= base_url('assets/landing/js/bootstrap.js'); ?>"></script>
-<?php if(! in_array($page, array('login','create','reset_password'))) :?>
+<?php if(!in_array($page, array('login','create','reset_password'))) :?>
 <script src="<?= base_url('assets/landing/js/owl.carousel.min.js'); ?>"></script>
 <script src="<?= base_url('assets/landing/js/ionrangeslider.js'); ?>"></script>
 <script src="<?= base_url('assets/landing/js/jqzoom.js'); ?>"></script>
