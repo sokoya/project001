91258afd131d9fc3ae3558ab3ac61e32f16161f3
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-12-14 15:03:39 +0100

    Updated homepage

--- application/views/landing/home.php
+++ application/views/landing/home.php
@@ -150,7 +150,7 @@
 		left: -3px;
 		margin: 0;
 		font-size: 13px;
-		font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+		font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
 		font-weight: 600;
 	}
 
@@ -179,7 +179,7 @@
 	}
 
 	.max-card-product-price {
-		font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+		font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
 		font-size: 12px;
 		position: relative;
 		left: -3px;
@@ -637,6 +637,86 @@
 		<img src="<?= base_url('assets/landing/img/home/ad1.jpg'); ?> " width="100%">
 	</div>
 
+	<div class="gap"></div>
+	<div class="container">
+		<hr class="hr-text" data-content="Why Choose Us">
+		<div class="owl-carousel owl-loaded owl-nav-out" data-options='{"items":5,"loop":true,"nav":true}'>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/largest.png'); ?>" alt="Largest"
+						 title="Largest in the market"/>
+					<h5 class="banner-category-title">Largest</h5>
+					<p class="banner-category-desc">Range of products to choose from.</p>
+				</a>
+			</div>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/carrito-shopper.png'); ?>"
+						 alt="<?= lang('app_name'); ?> Shopper" title="<?= lang('app_name'); ?> Shopper"/>
+					<h5 class="banner-category-title">Jewelry</h5>
+					<p class="banner-category-desc">Automated shopping assistant</p>
+				</a>
+			</div>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/secured-payment.png'); ?>"
+						 alt="secured-payment" title="secured-payment"/>
+					<h5 class="banner-category-title">Secured Payment</h5>
+					<p class="banner-category-desc">On all your debit/credit cards</p>
+				</a>
+			</div>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/incredible-discounts.png'); ?>"
+						 alt="Incredible Discounts" title="Discounts"/>
+					<h5 class="banner-category-title">Incredible Discounts</h5>
+					<p class="banner-category-desc">On all products, all season.</p>
+				</a>
+			</div>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/pay-on-delivery.png') ?>"
+						 alt="Pay on delivery" title="Pay on delivery"/>
+					<h5 class="banner-category-title">Pay On Delivery</h5>
+					<p class="banner-category-desc">On all products</p>
+				</a>
+			</div>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/fast-delivery.png') ?>"
+						 alt="Fast Delivery"
+						 title="Fast Delivery"/>
+					<h5 class="banner-category-title">Fast Delivery</h5>
+					<p class="banner-category-desc">Mationwide Same Day</p>
+				</a>
+			</div>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/genuine-brands.png') ?>"
+						 alt="Genuine Brands"
+						 title="Genuine Brands"/>
+					<h5 class="banner-category-title">Genuine Brands</h5>
+					<p class="banner-category-desc">From all over the world</p>
+				</a>
+			</div>
+			<div class="owl-item">
+				<a class="banner-category owl-item-slide" href="#">
+					<img class="banner-category-img"
+						 src="<?= base_url('assets/landing/img/why-onitshamarket/247.png') ?>"
+						 alt="247 Active Support" title="247 Active Support"/>
+					<h5 class="banner-category-title">24/7</h5>
+					<p class="banner-category-desc">Customer Agents Waiting To Help You.</p>
+				</a>
+			</div>
+		</div>
+	</div>
 
 	<?php if ($this->agent->is_mobile()) : ?>
 		<?php $this->load->view('landing/resources/mobile/mobile-footer'); ?>
