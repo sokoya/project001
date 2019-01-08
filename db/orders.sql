e0d8db31a30f04dfb2edb75c13cf2696597f9450
Sokoya Philip <sokoyaphilip@yahoo.com>
2019-01-08 11:12:33 +0100

    Update orders.sql

--- db/orders.sql
+++ db/orders.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 20, 2018 at 03:17 PM
+-- Generation Time: Jan 08, 2019 at 10:11 AM
 -- Server version: 5.7.11
--- PHP Version: 7.2.7
+-- PHP Version: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,10 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `order_code` varchar(10) NOT NULL,
-  `seller_id` int(11) NOT NULL,
+  `order_code` bigint(10) NOT NULL,
+  `tracking_id` bigint(20) NOT NULL,
   `buyer_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
+  `seller_id` int(11) NOT NULL,
+  `product_id` bigint(11) NOT NULL,
   `billing_address_id` int(11) NOT NULL,
   `product_variation_id` int(11) NOT NULL,
   `coupon_id` int(11) NOT NULL,
@@ -39,20 +40,22 @@ CREATE TABLE `orders` (
   `pickup_location_id` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
   `amount` decimal(10,0) NOT NULL,
-  `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(255) NOT NULL
+  `order_date` datetime NOT NULL,
+  `status` tinytext NOT NULL,
+  `active_status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Orders Table';
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `order_code`, `seller_id`, `buyer_id`, `product_id`, `billing_address_id`, `product_variation_id`, `coupon_id`, `payment_method`, `pickup_location_id`, `qty`, `amount`, `order_date`, `status`) VALUES
-(1, '431658', 1, 2, 1, 0, 0, 0, 0, 0, 1, '0', '2018-10-16 12:51:23', 'ordered'),
-(2, '431658', 1, 2, 2, 0, 0, 0, 0, 0, 1, '0', '2018-10-16 12:51:23', 'ordered'),
-(3, '398156', 1, 2, 1, 0, 0, 0, 0, 0, 3, '0', '2018-10-16 14:18:56', 'ordered'),
-(4, '874265', 3, 3, 4, 0, 0, 0, 0, 0, 1, '90000', '2018-11-05 10:49:48', 'ordered'),
-(6, '73862195', 1, 5, 2, 7, 2, 0, 1, 0, 2, '600000', '2018-11-20 15:14:31', 'ordered');
+INSERT INTO `orders` (`id`, `order_code`, `tracking_id`, `buyer_id`, `seller_id`, `product_id`, `billing_address_id`, `product_variation_id`, `coupon_id`, `payment_method`, `pickup_location_id`, `qty`, `amount`, `order_date`, `status`, `active_status`) VALUES
+(6, 73862195, 0, 3, 3, 2, 7, 2, 0, 1, 0, 2, '600000', '2018-11-20 15:14:31', '{"processing":{"msg":"Your order payment is processing","datetime":"2018-12-10 16:20:58"}}', 'pending'),
+(7, 73862195, 0, 3, 3, 4, 3, 2, 0, 1, 3, 2, '600000', '2018-11-21 15:14:31', '{"processing":{"msg":"Your order payment is processing","datetime":"2018-12-10 16:20:58"}}', 'pending'),
+(8, 46189573, 0, 3, 0, 2, 6, 2, 0, 1, 0, 1, '300000', '2018-12-10 13:57:42', '{"processing":{"msg":"Your order payment is processing","datetime":"2018-12-10 16:20:58"}}', 'pending'),
+(9, 29856714, 0, 3, 3, 2, 4, 2, 0, 1, 0, 1, '300000', '2018-12-10 14:07:00', '{"processing":{"msg":"Your order payment is processing","datetime":"2018-12-10 16:20:58"}}', 'pending'),
+(10, 85936421, 0, 3, 3, 2, 5, 2, 0, 1, 0, 1, '300000', '2018-12-10 15:59:39', '{"processing":{"msg":"Your order payment is processing","datetime":"2018-12-10 16:20:58"}}', 'pending'),
+(11, 14782539, 0, 3, 3, 2, 6, 2, 0, 1, 0, 1, '300000', '2018-12-10 16:20:58', '{"processing":{"msg":"Your order payment is processing","datetime":"2018-12-10 16:20:58"}}', 'pending');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +75,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
