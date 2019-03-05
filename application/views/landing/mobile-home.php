6f448171039a1f9755769149b8ef7157b0eae180
Adedoyin Skilz <michaeladekoniye@gmail.com>
2019-03-05 16:03:18 +0100

    mobile hom

--- application/views/landing/mobile-home.php
+++ application/views/landing/mobile-home.php
@@ -123,7 +123,85 @@
             <p class="text-center">Onitshamarket.com is a dynamic e-commerce platform that is designed to provide the most convenient experience for buying and selling in Nigeria. Beyond online shop; our unique focus of creating a virtual mall that interfaces with the largest market in Africa gives us an advantage over the competition. <br /> Giving customers the benefit of buying and selling quality and genuine goods from the biggest market in Africa, in the comfort of wherever they may be and having it delivered to them within a 24hrs lead time. </p>
         </div>
     </div>
-    <?php $this->load->view('landing/resources/why-onitshamarket'); ?>
+    <div class="container" style="padding:40px;">
+        <hr class="hr-text" data-content="Why Choose Us">
+        <div class="owl-carousel owl-loaded owl-nav-out" data-options='{"items":5,"loop":true,"nav":true}'>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/largest.png'); ?>" alt="Largest"
+                         title="Largest in the market"/>
+                    <h5 class="banner-category-title">Largest</h5>
+                    <p class="banner-category-desc">Range of products to choose from.</p>
+                </a>
+            </div>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/carrito-shopper.png'); ?>"
+                         alt="<?= lang('app_name'); ?> Shopper" title="<?= lang('app_name'); ?> Shopper"/>
+                    <h5 class="banner-category-title">OM Shopper</h5>
+                    <p class="banner-category-desc">Automated shopping assistant</p>
+                </a>
+            </div>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/secured-payment.png'); ?>"
+                         alt="secured-payment" title="secured-payment"/>
+                    <h5 class="banner-category-title">Secured Payment</h5>
+                    <p class="banner-category-desc">On all your debit/credit cards</p>
+                </a>
+            </div>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/incredible-discounts.png'); ?>"
+                         alt="Incredible Discounts" title="Discounts"/>
+                    <h5 class="banner-category-title">Incredible Discounts</h5>
+                    <p class="banner-category-desc">On all products, all season.</p>
+                </a>
+            </div>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/pay-on-delivery.png') ?>"
+                         alt="Pay on delivery" title="Pay on delivery"/>
+                    <h5 class="banner-category-title">Pay On Delivery</h5>
+                    <p class="banner-category-desc">On all products</p>
+                </a>
+            </div>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/fast-delivery.png') ?>"
+                         alt="Fast Delivery"
+                         title="Fast Delivery"/>
+                    <h5 class="banner-category-title">Fast Delivery</h5>
+                    <p class="banner-category-desc">Mationwide Same Day</p>
+                </a>
+            </div>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/genuine-brands.png') ?>"
+                         alt="Genuine Brands"
+                         title="Genuine Brands"/>
+                    <h5 class="banner-category-title">Genuine Brands</h5>
+                    <p class="banner-category-desc">From all over the world</p>
+                </a>
+            </div>
+            <div class="owl-item">
+                <a class="banner-category owl-item-slide" href="#">
+                    <img class="banner-category-img"
+                         src="<?= base_url('assets/img/why-onitshamarket/247.png') ?>"
+                         alt="247 Active Support" title="247 Active Support"/>
+                    <h5 class="banner-category-title">24/7</h5>
+                    <p class="banner-category-desc">Customer Agents Waiting To Help You.</p>
+                </a>
+            </div>
+        </div>
+    </div>
 
 
     <?php $this->load->view('landing/resources/mobile/mobile-footer'); ?>
