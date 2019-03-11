4c9c94ceb1e0a55b57562d417c4017187fe52127
jaytechx <chidij75@gmail.com>
2019-03-11 10:25:30 +0100

    Merge

--- application/views/landing/resources/head_img.php
+++ application/views/landing/resources/head_img.php
@@ -1,4 +1,4 @@
 <div class=" hide_mobile navbar-before-inverse">
-    <img class="ad-banner" src="<?= base_url('assets/img/home/b.webp'); ?>">
+    <img class="ad-banner" src="<?= base_url('assets/img/home/top_banner.png'); ?>">
     <span id="close-banner"><i class="fas fa-times-circle"></i></span>
 </div>
