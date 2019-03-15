6d4cdbcddb866316d746e6c43beaa2f5a985a385
sokoyaphilip <sokoyaphilip@yahoo.com>
2019-03-15 17:40:37 +0100

    Remove brace

--- application/views/landing/mobile-search.php
+++ application/views/landing/mobile-search.php
@@ -242,7 +242,7 @@
                     </div>
                 </div>
                 <div class="col-xs-12 ">
-}
+
                     <div class="row row-sm-gap" data-gutter="10">
                         <?php if($products) :foreach ($products as $product) : ?>
                             <div class="col-md-3">
