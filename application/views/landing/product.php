adc09eedb2f8e92a941ff96868fa02795bd191a6
Sokoya Philip <philo4u2c@gmail.com>
2019-03-28 13:07:57 +0100

    fetch

--- application/views/landing/product.php
+++ application/views/landing/product.php
@@ -293,14 +293,14 @@
                                     <?= form_open('', 'id="variation-form"'); ?>
                                     <div class="row">
                                         <?php if (count($variations) > 0) : ?>
-                                            <div class="col-md-7">
-                                                <?php if(in_array(strtolower($category_detail->name), array('fashion', 'clothing', 'jewelry', 'shoes'))) :?>
-                                                    <div class="row">
-                                                        <div class="col-md-3 ">
-                                                            <h5 class="custom-product-page-option-title">Size / Color:</h5>
-                                                        </div>
+                                            <?php if(in_array(strtolower($category_detail->name), array('fashion', 'clothing', 'jewelry', 'shoes'))) :?>
+                                                <div class="row">
+                                                    <div class="col-md-3 ">
+                                                        <h5 class="custom-product-page-option-title">Size / Color:</h5>
                                                     </div>
-                                                <?php endif?>
+                                                </div>
+                                            <?php endif?>
+                                            <div class="col-md-7">
                                                 <div class="row">
                                                     <div class="col-md-9">
                                                         <div class="row variation-option-list">
