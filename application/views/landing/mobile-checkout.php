d077f2394b42568ffda22fb934bb2324eeb28455
Sokoya Philip <philo4u2c@gmail.com>
2019-04-01 14:34:57 +0100

    commit

--- application/views/landing/mobile-checkout.php
+++ application/views/landing/mobile-checkout.php
@@ -105,12 +105,13 @@
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-md-6">
+                                        <div class="col-md-4">
                                             <button type="button" class="btn btn-primary btn-block create-address-btn">
                                                 Submit
                                             </button>
                                         </div>
-                                        <div class="col-md-6">
+
+                                        <div class="col-md-push-4">
                                             <button type="button" class="btn btn-warning btn-block cancel-btn">
                                                 Cancel
                                             </button>
