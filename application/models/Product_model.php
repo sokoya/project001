f4febe86b4b7e9b6ff78d3b57560cb0215969810
Sokoya Philip <philo4u2c@gmail.com>
2019-04-15 16:18:55 +0100

    Parent CAtegory

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -182,11 +182,11 @@ Class Product_model extends CI_Model{
         // Select category
         $GLOBALS['array_variable'] = array();
 //        $temp = $this->db->query("SELECT pid FROM categories WHERE id = {$id}")->row();
-        $select_category = "SELECT id, slug FROM categories WHERE id = {$id}";
+        $select_category = "SELECT pid, slug FROM categories WHERE id = {$id}";
         $result = $this->db->query($select_category);
 
         if( $result->num_rows() >= 1 ){
-            $pid = $result->row()->id;
+            $pid = $result->row()->pid;
             $this->parent_recurssive( $pid );
             $array = array_filter($GLOBALS['array_variable']);
             $it = new RecursiveIteratorIterator(new RecursiveArrayIterator($array));
@@ -207,7 +207,6 @@ Class Product_model extends CI_Model{
         $category_pid = $pid;
         $total_categories = $this->db->get('categories')->result_array();
         $count = count( $total_categories );
-
         $data = array();
         for ($i=0; $i < $count; $i++) {
             if( $total_categories[$i]['id'] == $category_pid ){
