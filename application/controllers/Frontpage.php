a30a1b06e3bd18097ba8747ff108df30073d80d1
Sokoya Philip <philo4u2c@gmail.com>
2019-04-16 10:04:54 +0100

    Commit for show

--- application/controllers/Frontpage.php
+++ application/controllers/Frontpage.php
@@ -22,6 +22,7 @@ class Frontpage extends MY_Controller {
                 $review['user_id'] = $this->session->userdata('logged_id');
                 $review['username'] = ucfirst($page_data['profile']->first_name) . ' ' . $page_data['profile']->last_name;
             }
+//            var_dump( $row );exit;
         }
 
         $page_data['review'] = json_encode($review, JSON_UNESCAPED_SLASHES);
