a7629c3cfb44b787159de4b9247378d7df7797b0
Sokoya Philip <philo4u2c@gmail.com>
2019-04-16 09:55:57 +0100

    Year

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -497,12 +497,12 @@ Class User_model extends CI_Model
 
 
     function get_order_for_review($buyer_id){
-        $this_month = date('m', strtotime('this month'));
+        $this_month = date('y', strtotime('this year'));
         $query = "SELECT o.id, p.product_name, p.id product_id, g.image_name FROM orders o 
         JOIN products p ON (o.product_id = p.id)
         JOIN product_gallery g ON(g.product_id = o.product_id AND g.featured_image = 1 )
         WHERE NOT EXISTS (select 1 from product_review r where p.id = r.product_id ) 
-        AND o.buyer_id = {$buyer_id} AND (o.payment_made = 'success' OR o.active_status = 'completed') AND (MONTH(o.order_date) = '{$this_month}')
+        AND o.buyer_id = {$buyer_id} AND (o.payment_made = 'success' OR o.active_status = 'completed') AND (YEAR(o.order_date) = '{$this_month}')
         GROUP BY o.order_code ORDER BY o.id DESC LIMIT 1";
         return $this->db->query($query)->row();
     }
