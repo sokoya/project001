e41b00ce31ce78e18c637320b01c5014973cf666
Sokoya Philip <philo4u2c@gmail.com>
2019-04-30 17:15:12 +0100

    A2HS

--- application/views/landing/home.php
+++ application/views/landing/home.php
@@ -681,6 +681,7 @@
         }
         <?php endif?>
     });
+
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function () {
             navigator.serviceWorker.register("<?= base_url('sw.js');?>")
@@ -691,6 +692,35 @@
                 });
         });
     }
+
+    let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI notify the user they can add to home screen
+        btnAdd.style.display = 'block';
+    });
+
+    btnAdd.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        btnAdd.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+            .then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+    });
+
+
 </script>
 <script src="<?= $this->user->auto_version('assets/js/rating.js'); ?>"></script>
 </body>
