46c94681bb6fc9aada0e4f142a8503adba6b0344
Sokoya Philip <philo4u2c@gmail.com>
2019-06-06 13:27:12 +0100

    here

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -676,7 +676,12 @@ Class Product_model extends CI_Model{
         if( $queries['product_name'] ) {
             $product_name = cleanit( $queries['product_name']);
             $product_name = preg_replace("/[^a-z]/", '', $product_name);
-            $select_query .= " WHERE p.product_status = 'approved' AND p.product_name LIKE '%{$product_name}%' ";
+            $select_query .= " WHERE p.product_status = 'approved' AND p.product_name LIKE '%{$product_name}%'";
+
+            $keys = explode(" ", $product_name );
+            foreach ( $keys as $k ){
+                $select_query .= " OR p.product_name LIKE '%{$k}%'";
+            }
         }
         if( $queries['category'] && !empty($queries['category'])){
             $id = $this->category_id( $queries['category'] );
