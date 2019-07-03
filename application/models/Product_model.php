679655c44964dc92a3759fe056b07e9c586d4c1b
Sokoya Philip <philo4u2c@gmail.com>
2019-07-03 18:24:49 +0100

    ficing search

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -696,6 +696,7 @@ Class Product_model extends CI_Model{
         if( $queries['product_name'] ) {
             $product_name = xss_clean( $queries['product_name']);
             $product_name = preg_replace("/[^a-z0-9 ]/", ' ', $product_name);
+            die( $product_name );
             $select_query .= " WHERE p.product_status = 'approved' AND p.product_name LIKE '%{$product_name}%'";
 //            $keys = explode(" ", $product_name );
 //            foreach ( $keys as $k ){
