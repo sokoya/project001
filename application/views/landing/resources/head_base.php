6c3b4b62d17d8b11b5c776e5c681f3f38f4e3e17
Sokoya Philip <philo4u2c@gmail.com>
2019-07-19 10:45:04 +0100

    Frontpage re-design

--- application/views/landing/resources/head_base.php
+++ application/views/landing/resources/head_base.php
@@ -62,6 +62,7 @@
     <meta name="yandex-verification" content="5e0c9cc8260f049f" />
 
 
+
     <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/assets/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/apple-icon-72x72.png">
