0482b8414065c36c904193446366fedd1ed11200
Sokoya Philip <philo4u2c@gmail.com>
2019-07-19 11:17:22 +0100

    Frontpage re-design

--- application/views/landing/resources/head_base.php
+++ application/views/landing/resources/head_base.php
@@ -63,6 +63,7 @@
 
 
 
+    
     <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/assets/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/apple-icon-72x72.png">
