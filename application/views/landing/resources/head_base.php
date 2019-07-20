167f9067eb05582c11f9b5de5a5bc865e38eecb1
Sokoya Philip <philo4u2c@gmail.com>
2019-07-20 21:56:15 +0100

    COmmit

--- application/views/landing/resources/head_base.php
+++ application/views/landing/resources/head_base.php
@@ -50,9 +50,7 @@
         <meta property="og:type" content="product"/>
         <meta property="og:image"
               content="<?= ($featured_image->image_name) ? PRODUCTS_IMAGE_PATH . $featured_image->image_name : ''; ?>" />
-        <meta property="og:description"
-              itemprop="description"
-              content="<?= isset( $product) ? html_entity_decode( $product->product_description) : lang('site_description') ?>" />
+
         <meta property="og:site_name" content="<?= lang('app_name'); ?>"/>
         <meta property="og:url" content="<?= current_url(); ?>"/>
         <meta property="og:image:width" content="279">
