70787ca3a6c7625181b7baae769ab37b89cab483
Sokoya Philip <philo4u2c@gmail.com>
2019-07-24 21:02:15 +0100

    commit

--- application/views/landing/mobile-search.php
+++ application/views/landing/mobile-search.php
@@ -216,11 +216,11 @@
     </script>
     <script src="<?= $this->user->auto_version('assets/js/search.js'); ?>"></script>
     <script>
-        window.addEventListener('load', function () {
+        indow.addEventListener('load', function () {
             let allimages = document.getElementsByTagName('img');
             for (let i = 0; i < allimages.length; i++) {
-                allimages[i].setAttribute('src', '');
                 if (allimages[i].getAttribute('data-src')) {
+                    allimages[i].setAttribute('src', '');
                     allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
                 }
             }
