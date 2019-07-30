2d491d4f7d7f0cf57a411abb2044c6d67a2586b3
Sokoya Philip <philo4u2c@gmail.com>
2019-07-30 13:30:20 +0100

    commit

--- application/views/landing/home.php
+++ application/views/landing/home.php
@@ -545,18 +545,6 @@
 
 </script>
 <script src="<?= $this->user->auto_version('assets/js/rating.js'); ?>"></script>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/5d402e99e5ae967ef80d7cc6/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
+
 </body>
 </html>
