566c9d6efb74a8d65276db4b1a0cc529ce7a2b40
Sokoya Philip <philo4u2c@gmail.com>
2019-07-31 13:07:21 +0100

    commit for lotta changes

--- application/views/landing/mobile-product.php
+++ application/views/landing/mobile-product.php
@@ -709,7 +709,7 @@
             }
         }
         let x = 1;
-        $('.overview_data').find('img').each(function(){
+        $('.overview_data > .container > div').find('img').each(function(){
             console.log( x ); x++;
             $(this).css({'max-width' : '40%', 'position' : 'center'});
         });
